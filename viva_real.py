90a700553ebd92b6fb0778dbbcab0c5edf9c145d
Vinicius Alves <vini.costeira@gmail.com>
2024-12-03 21:39:28 -0300

    adicionado web scraping e listagem

--- viva_real.py
+++ viva_real.py
@@ -1,4 +1,7 @@
-# [15:59, 02/12/2024] Jou:  - Pesquisa no site da viva real. Vai ter uma planilha de input, com o nome do bairro e da cidade, com o tipo de contrato(aluguel ou compra)  e você tem que capturar: Valor, Area, Cresci, Imobiliaria e telefone. Depois de pesquisar, tem que ir anuncio em anuncio e pegando as fotos e criar um PDF com as Fotos e tirar um print da pagina do anuncio. Precisa pegar a data e a hora que foi feita a pesquisa e o link de cada resultado que retornou
+# [15:59, 02/12/2024] Jou:  - Pesquisa no site da viva real. Vai ter uma planilha de input, com o nome do bairro e da cidade, com o tipo de contrato(aluguel ou compra) 
+# e você tem que capturar: Valor, Area, Cresci, Imobiliaria e telefone.
+# Depois de pesquisar, tem que ir anuncio em anuncio e pegando as fotos e criar um PDF com as Fotos e tirar um print da pagina do anuncio. 
+# Precisa pegar a data e a hora que foi feita a pesquisa e o link de cada resultado que retornou
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -40,20 +43,61 @@ for linha in range(2, sheet.max_row +1):
         contrato_comprar = navegador.find_element(By.XPATH, './/button[@data-cy="home-buy-tb-tab"]').click()
         sleep(1)
 
-    
+    #Localizando campo de pesquisa e preenchendo.
     preencher = navegador.find_element(By.XPATH, './/input[@type="text"]')
     preencher.clear()
     preencher.send_keys(f"{nome_bairro} {nome_cidade}")
     sleep(5)
-      
+    
+    # Aguardando o elemento aparecer
     checkbox = WebDriverWait(navegador, 5).until(
         EC.visibility_of_element_located((By.XPATH, './/input[@id="l-checkbox-4"]')))
     
+    # Clica no elemento
     checkbox.click()
     sleep(1)
-        
+    
+    # Clica no botão de buscar
     botao_buscar = navegador.find_element(By.XPATH, './/button[@type="submit"]').click()
-    sleep(1)
+    sleep(5)
+    
+    # Criando variável que vai receber lista de imoveis
+    lista_imoveis = []
+    
+    # Localizando elemento pai
+    imoveis = navegador.find_elements(By.XPATH, './/div[@data-type="property"]')
+    aba_original = navegador.current_window_handle
+        
+    for imovel in imoveis:
+        acessar_anuncio = navegador.find_element(By.XPATH, './/div[@class="property-card__content"]').click()
+        sleep(3)
+        
+        valor_imovel = navegador.find_element(By. XPATH, './/p[@data-testid="price-info-value"]').text
+        area_imovel = navegador.find_element(By.XPATH, './/p[@itemprop="floorSize"]/span[@data-cy="ldp-propertyFeatures-txt"]').text
+        cresci = navegador.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div/div/div[1]/div/div/div[2]/p').text
+        imobiliaria = navegador.find_element(By.XPATH, '/html/body/div[2]/div[2]/div[1]/div/div/div/div[1]/div/div/div[2]/div/p').text
+        
+        botao_ver_telefone = navegador.find_element(By.XPATH, '/html/body/div[2]/div[1]/div[1]/div[2]/section/div[3]/button').click()
+        telefone1 = WebDriverWait(navegador, 5).until(EC.visibility_of_element_located((By.XPATH, './/a[@data-cy="lead-modalPhone-phonesList-txt"][1]')))
+        telefone2 = WebDriverWait(navegador, 5).until(EC.visibility_of_element_located((By.XPATH, './/a[@data-cy="lead-modalPhone-phonesList-txt"][2]')))
+        
+        url = navegador.current_url   
+        
+        lista_imoveis.append({'valor_imovel': valor_imovel, 'area_imovel': area_imovel, 'cresci': cresci, 'imobiliaria': imobiliaria, 'telefone1': telefone1, 'telefone2': telefone2, 'url': url})
+        
+        navegador.close()
+        
+        navegador.switch_to.window(aba_original)
+    
+    for imovel in lista_imoveis:
+        print(f"{imovel['valor_imovel']:<10}{imovel['area_imovel']:<5}{imovel['cresci']:<10}{imovel['imobiliaria']:<20}{imovel['telefone1']:<10}{imovel['telefone2']:<10}{imovel['url']:<50}")
+        print("-" * 70)
+    
+    
+    
+    
+    
+    
         
         
 
